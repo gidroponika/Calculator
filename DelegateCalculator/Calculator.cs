3119d5a0d13b480ba850ec69ca77f51a01d22f47
Maxim Gorodnichiy <myemail@my.com>
2023-08-18 13:36:33 +0300

    Добавьте файлы проекта.

---
+++ DelegateCalculator/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateCalculator
+{
+    internal static class Calculator
+    {
+        public static double Add(double a, double b) => a + b;
+
+        public static double Sub(double a, double b) => a - b;
+
+        public static double Mul(double a, double b) => a * b;
+
+        public static double Div(double a, double b)
+        {
+            if (b != 0)
+            {
+                return a / b;
+            }
+            else
+            {
+                Console.WriteLine("division by zero is not possible");
+                return 0;
+            }
+        }
+    }
+}
---
+++ DelegateCalculator/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace DelegateCalculator
+{
+    delegate double Calculate(double a, double b);
+    internal class Program
+    {
+        static void Main()
+        {
+            double result = default;
+            Calculate calc = null;
+
+            Console.Write("Enter first number ");
+            double q = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the symbol of operation ");
+            char operation = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Enter second number ");
+            double w = Convert.ToDouble(Console.ReadLine());
+
+
+            switch (operation)
+            {
+                case '+':
+                    calc = new Calculate(Calculator.Add);
+                    break;
+                case '*':
+                    calc = new Calculate(Calculator.Mul);
+                    break;
+                case '-':
+                    calc = new Calculate(Calculator.Sub);
+                    break;
+                case '/':
+                    calc = new Calculate(Calculator.Div);
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation");
+                    break;
+            }
+
+            result = calc.Invoke(q, w);
+            Console.WriteLine(result);
+        }
+    }
+}
